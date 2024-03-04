e0ac45a76d71b29f99ff1e5cd47dac29326c74c6
Christopher Cantu <chrischimi02@gmail.com>
2024-03-03 21:30:14 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const mainElement = document.getElementById('main');
+mainElement.remove()
+
+const newHeader = document.createElement("H1");
+newHeader.id = "victory";
+newHeader.textContent = "is the champion";
